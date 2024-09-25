b9c84443ff857895144c02ebd5b01e53340b2179
Jorge Diego Valdez Fonseca <99843870+diiegood@users.noreply.github.com>
2024-09-24 20:19:47 -0600

    Update funciones ultrabasicas python_2.py

--- funciones ultrabasicas python_2.py
+++ funciones ultrabasicas python_2.py
@@ -140,28 +140,153 @@ id (x), id(y)
 
 ###############################- Condicionales -###############################
 
+#logicas matematicas 
+
+a = 5
+b = 3
+a == b  #es igual a
+a != b #no es igual
+a < b #menor que 
+a <= b #menor igual que
+a > b #mayor que
+a >= b #mayor igual que 
+
+if 1 < 2:
+    print("uno menos que dos")
+    
+a = 10
+b = 25
+if b > a:
+    print("b es mayor que a")
 
 
+a = 33
+b = 33
+if b > a:
+    print("b es mayor que a")  #condiciona principal
+elif a == b :  #condicion secundaria por si no se cumple
+    print("a y b son iguales")
+
+a = 15
+if a < 10: 
+    print("a menos que 10")
+elif a > 20: 
+    print("a mayor que 20")
+else: 
+    print("a otro caso")
+
+x = 1 if (1>2) else 3 
+x
+
+#and sirve como operador logico para combinar condicionales
+
+a = 50
+b = 25
+c = 100
+
+if a > b and c > a:
+    print("ambas condiciones son variedad")
+
+#or sirve para combinar las condiciones 
+
+a1 = 50 
+a2 = 25
+a3 = 10
+
+if a > a1 or a2 < a3: 
+    print("al menos una de las condiciones es verdad")
+
+# not se usa para invertir el resultado de la declaracion condicional
+
+ab = 33
+ac = 200
+if not ab > b:
+    print("ab no es mayor que ac")
+
+#se puede tener declaraciones if dentro de otras se llaman anidadas
+
+x = 41 
+if x > 10:
+    print("Mas de 10,")
+if x  > 20:
+    print("y tambien mas de 20!")
+else: 
+    print("pero no por encima de 20.")
+    
+#Recopilar elementos a traves de bucles es tan comun en Python
+    
+out = [] #se crea una lista vacia
+for i in range (10): #rango de valores de 10
+    out.append (i**2) #funcion de i^2
+out #Nuestra lista ya no esta vacia
+
+15%15 #operador de modulo, busca residuos de division
+#como el residuo es 0 aparece el resultado
+
+5%2
+
+for numero in range (1,11):
+    if (numero %2 !=0):
+        print(numero)
+
+#Funciones  / bloques de codigo que generan un proceso, llamado parametro
+#devuelve resultado despues del proceso
+
+def buenas_tardes():
+    print("Hola, buenas tardes joven")
+
+buenas_tardes()
+
+#esta funcion esta programada para dar las buenas tardes
+
+activos = ["Amazon", "SP500", "Microsoft", "Nvidia", "Apple" ]
+stock_price = [220, 3560, 345, 120 * 10 , 157]
+def port_inv (activos, stock_price):
+  
+    print("Activos del portafolio: " + ", ".join(activos))
+    print("Y este es su precio: " + ", ".join(map(str, stock_price)))
+
+# Llama a la función
+port_inv(activos, stock_price)
+
+def mi_funcion(country = "Mexico"): #Parametro predeterminado
+    print("Yo soy de " + country)
+
+mi_funcion("Francia")
+mi_funcion("India")
+mi_funcion("Guatemala")
+mi_funcion("Brasil")
+
+def mercado_deuda(*instrumento):  #el asterisco es por si se desconoce el numero de argumentos
+    print("instumentos de inversion seguros" + instrumento[0]) 
+
+mercado_deuda("Cetes", "Treasure Bills", "BPAS") #tupla como argumento
+
+def mercado_deuda(plazo):
+    for x in plazo:
+        print(x)
+
+plazo = [28, 91, 180]
+
+mercado_deuda(plazo)
 
 
+tasa = ["diez punto treinta cinco", "diez punto cuarenta y cinco", "diez punto cincuenta y cinco"]
 
+def mercado_deuda(X):
+    return tasa + [X]
 
+print(mercado_deuda("tasa 28 días"))
+print(mercado_deuda("tasa 91 días"))
+print(mercado_deuda("tasa 180 días"))
 
+#segundo ejemplo 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def mi_funcion(x):
+   return 3 * x #Definimos valores como retorno
+print(mi_funcion(3))
+print(mi_funcion(6))
+print(mi_funcion(9))
 
 
 
